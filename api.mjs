9368ac83e9f2995cdabda08f129500b1a924836f
Woonki Moon <woonki.moon@gmail.com>
2021-09-06 08:42:07 +0900

    fix: 토/일요일에도 fetch하도록 수정

--- api.mjs
+++ api.mjs
@@ -4,16 +4,23 @@ const getMealInfo = (schoolCode, { year, month, date }) => {
   fetch(requestUrl)
     .then((res) => res.json())
     .then((json) => {
-      let meal = json["mealServiceDietInfo"][1].row[0].DDISH_NM.replace(
-        /[0-9]/g,
-        ""
-      ); // 불필요한 숫자를 제거한다.
-      meal = meal.replace(/\./g, ""); // 불필요한 마침표를 제거한다.
-      meal = meal.replace("우유", ""); // 우유는 표시하지 않는다.
-      document.title = `${json["mealServiceDietInfo"][1].row[0].SCHUL_NM}의 급식`;
-      document.write(
-        `<h3 style="font-family: 'SBAggroL'; text-align:center; padding-top: 60px;">${meal}</h3>`
-      );
+      if (!("mealServiceDietInfo" in json)) {
+        document.write(
+          `<h1 style="font-family: 'SBAggroL'; text-align:center; padding-top: 160px;">오늘은 급식이 <br>없는 날입니다.</h1>`
+        );
+        document.title = `급식`;
+      } else {
+        let meal = json["mealServiceDietInfo"][1].row[0].DDISH_NM.replace(
+          /[0-9]/g,
+          ""
+        ); // 불필요한 숫자를 제거한다.
+        meal = meal.replace(/\./g, ""); // 불필요한 마침표를 제거한다.
+        meal = meal.replace("우유", ""); // 우유는 표시하지 않는다.
+        document.title = `${json["mealServiceDietInfo"][1].row[0].SCHUL_NM}의 급식`;
+        document.write(
+          `<h3 style="font-family: 'SBAggroL'; text-align:center; padding-top: 60px;">${meal}</h3>`
+        );
+      }
     })
     .catch((err) => {
       console.log(err);
--- app.js
+++ app.js
@@ -14,14 +14,4 @@ while (!schoolCode) {
   }
 }
 
-const { day } = getDate();
-
-// 먼저 요일을 체크해서 토/일요일인 경우 fetch를 호출하지 않는다.
-if (day === "SAT" || day === "SUN") {
-  document.write(
-    `<h1 style="font-family: 'SBAggroL'; text-align:center; padding-top: 160px;">오늘은 급식이 <br>없는 날입니다.</h1>`
-  );
-  document.title = `급식`;
-} else {
-  getMealInfo(schoolCode, getDate());
-}
+getMealInfo(schoolCode, getDate());
