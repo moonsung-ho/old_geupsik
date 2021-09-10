6a6bd31dc84aafb4187f53b5c3f0de7d0c30c84d
sungho0205 <moon@7959.ml>
2021-09-10 15:58:01 +0900

    d

--- api.mjs
+++ api.mjs
@@ -1,14 +1,14 @@
 const getMealInfo = (schoolCode, { year, month, date }) => {
+  const dateInput = document.querySelector('input');
+  dateInput.value = `${year}-${month}-${date}`
   const requestUrl = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=4c1690204c08404ca7f1775720f17054&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=${schoolCode}&MLSV_YMD=${year}${month}${date}`;
-const today = document.getElementsByClassName('today')
+  const today = document.getElementsByClassName('today')[0];
   fetch(requestUrl)
     .then((res) => res.json())
     .then((json) => {
       if (!('mealServiceDietInfo' in json)) {
-        document.write(
-          `<h1 style="font-family: 'SBAggroL'; text-align:center; padding-top: 160px;">오늘은 급식이 <br>없는 날입니다.</h1>`,
-        );
-        document.title = `급식`;
+        (today.innerHTML = `<h1 style="font-family: 'SBAggroL'; text-align:center; padding-top: 160px;">오늘은 급식이 <br>없는 날입니다.</h1>`),
+          (document.title = `급식`);
       } else {
         let meal = json['mealServiceDietInfo'][1].row[0].DDISH_NM.replace(
           /[0-9]/g,
@@ -17,9 +17,7 @@ const today = document.getElementsByClassName('today')
         meal = meal.replace(/\./g, ''); // 불필요한 마침표를 제거한다.
         meal = meal.replace('우유', ''); // 우유는 표시하지 않는다.
         document.title = `${json['mealServiceDietInfo'][1].row[0].SCHUL_NM}의 급식`;
-        document.write(
-          `<h1 style="font-family: 'SBAggroL'; text-align:center; padding-top: 60px;">${meal}</h1>`,
-        );
+        today.innerHTML = `<h2 style="font-family: 'SBAggroL'; text-align:center;">${meal}</h2>`;
       }
     })
     .catch((err) => {
@@ -32,5 +30,4 @@ const today = document.getElementsByClassName('today')
       }
     });
 };
-
 export { getMealInfo };
