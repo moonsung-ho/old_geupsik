e81871deabbfe9ced866bdd8be01b5d4c3a715c9
sungho0205 <moon@7959.ml>
2021-09-13 11:09:06 +0900

    ios 유저의 경우 alert로 홈 화면에 추가

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if (
   navigator.userAgent.indexOf('iPad') != -1
 ) {
   if (!window.navigator.standalone) {
-    if (localStorage.getItem('homescreenbanner')) {
+    if (!localStorage.getItem('homescreenbanner')) {
       alert('밑 공유 버튼을 누르고 스크롤을 내려서 앱을 홈 화면에 추가하세요!');
       localStorage.setItem('homescreenbanner', true);
     }
