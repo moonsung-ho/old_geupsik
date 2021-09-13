ef985651aa1941baa2c4c4dc0b44aac08f0526a9
sungho0205 <moon@7959.ml>
2021-09-13 10:24:48 +0900

    pwa 준비

--- app.js
+++ app.js
@@ -3,25 +3,13 @@ import { getSchoolCode, setSchoolCode } from './db.mjs';
 import { getDateStr, getDate } from './utils.mjs';
 import { getMealInfo } from './api.mjs';
 import { parseDateStr } from './utils.mjs';
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    deferredPrompt = null;
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+      navigator.serviceWorker.register("/service-worker.js");
   });
-});
+}
+
+
 const dateInput = document.querySelector('#select-date');
 dateInput.value = getDateStr();
 let geupsik = localStorage.getItem('geupsik');
