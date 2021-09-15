f6b0ef89b34e305bad0d42aad6296515971de951
sungho0205 <moon@7959.ml>
2021-09-16 08:03:19 +0900

    스와이프로 날짜바꾹;

--- app.js
+++ app.js
@@ -1,8 +1,81 @@
 import schools from './schools.mjs';
 import { getSchoolCode, setSchoolCode } from './db.mjs';
-import { getDateStr, getDate } from './utils.mjs';
+import {
+  getDateStr,
+  getDate,
+  getNextDate,
+  getDateNStr,
+  getPrevDate,
+  getDatePStr,
+} from './utils.mjs';
 import { getMealInfo } from './api.mjs';
 import { parseDateStr } from './utils.mjs';
+let dateInput = document.querySelector('#select-date');
+dateInput.value = getDateStr();
+let schoolCode = getSchoolCode();
+while (!schoolCode) {
+  const input = prompt(
+    '학교 이름을 입력하세요. (ex.답십리초, 신길중, 신도림고)',
+  );
+  schoolCode = schools[input];
+  if (schoolCode) {
+    setSchoolCode(schoolCode);
+  }
+}
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || // browser API
+    evt.originalEvent.touches
+  ); // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      /* left swipe */
+      let datesplit = dateInput.value.split('-');
+      getMealInfo(
+        schoolCode,
+        getNextDate(datesplit[0], datesplit[1], datesplit[2]),
+      );
+      dateInput.value = getDateNStr(datesplit[0], datesplit[1], datesplit[2]);
+    } else {
+      /* right swipe */
+      let datesplit = dateInput.value.split('-');
+      getMealInfo(
+        schoolCode,
+        getPrevDate(datesplit[0], datesplit[1], datesplit[2]),
+      );
+      dateInput.value = getDatePStr(datesplit[0], datesplit[1], datesplit[2]);
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -23,26 +96,12 @@ if ('serviceWorker' in navigator) {
     }
   }
 }
-
-const dateInput = document.querySelector('#select-date');
-dateInput.value = getDateStr();
 let geupsik = localStorage.getItem('geupsik');
 localStorage.setItem('geupsik', geupsik * 1 + 1);
 
 document.querySelector('#select-date').onchange = function () {
-  const dayChosen = parseDateStr(dateInput.value);
+  let dayChosen = parseDateStr(dateInput.value);
   getMealInfo(schoolCode, dayChosen);
 };
-
-let schoolCode = getSchoolCode();
-while (!schoolCode) {
-  const input = prompt(
-    '학교 이름을 입력하세요. (ex.답십리초, 신길중, 신도림고)',
-  );
-  schoolCode = schools[input];
-  if (schoolCode) {
-    setSchoolCode(schoolCode);
-  }
-}
 
 getMealInfo(schoolCode, getDate());
--- utils.mjs
+++ utils.mjs
@@ -1,21 +1,65 @@
 const getDate = () => {
-  const today = new Date();
+  let today = new Date();
 
-  const year = today.getFullYear(); // 년도
+  let year = today.getFullYear(); // 년도
   let month = today.getMonth() + 1; // 월
   if (month < 10) {
     month = `0${month}`;
   }
   let date = today.getDate(); // 날짜
   if (date < 10) {
-    date = `0${date}`;
+    date = `0${date}` * 1;
   }
 
   let day = today.getDay(); // 요일
-  const WEEKDAY = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const WEEKDAY = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
   day = WEEKDAY[day];
 
   return { year, month, date, day };
+};
+const getPrevDate = (nyear, nmonth, ndate) => {
+  let today = new Date(nyear, nmonth, ndate);
+  let prevDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() - 1,
+  );
+  let year = prevDate.getFullYear();
+  let month = `${prevDate.getMonth()}`;
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  let date = prevDate.getDate();
+  if (date < 10) {
+    date = `0${date}` * 1;
+  }
+  return { year, month, date };
+};
+const getDatePStr = (nyear, nmonth, ndate) => {
+  const { year, month, date } = getPrevDate(nyear, nmonth, ndate);
+  return `${year}-${month}-${date}`;
+};
+const getNextDate = (nyear, nmonth, ndate) => {
+  let today = new Date(nyear, nmonth, ndate);
+  let nextDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 1,
+  );
+  let year = nextDate.getFullYear();
+  let month = `${nextDate.getMonth()}`;
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  let date = nextDate.getDate();
+  if (date < 10) {
+    date = `0${date}` * 1;
+  }
+  return { year, month, date };
+};
+const getDateNStr = (nyear, nmonth, ndate) => {
+  const { year, month, date } = getNextDate(nyear, nmonth, ndate);
+  return `${year}-${month}-${date}`;
 };
 
 const getDateStr = () => {
@@ -24,11 +68,19 @@ const getDateStr = () => {
 };
 
 const parseDateStr = (s) => {
-  const splitted = s.split("-");
-  const year = splitted[0];
+  const splitted = s.split('-');
+  const year = splitted[0] * 1;
   let month = splitted[1];
-  let date = splitted[2];
+  let date = splitted[2] * 1;
   return { year, month, date };
 };
 
-export { getDate, getDateStr, parseDateStr };
+export {
+  getDate,
+  getDateStr,
+  parseDateStr,
+  getNextDate,
+  getDateNStr,
+  getPrevDate,
+  getDatePStr,
+};
