1c0ea0db031f102e9a1eb2ec00fd615217464140
sungho0205 <moon@7959.ml>
2021-09-22 20:07:09 +0900

    d

--- api.mjs
+++ api.mjs
@@ -1,8 +1,9 @@
 const getMealInfo = (schoolCode, { year, month, date }) => {
+  let easterEgg = 0;
   const requestUrl = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=4c1690204c08404ca7f1775720f17054&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=${schoolCode}&MLSV_YMD=${year}${month}${date}`;
   const today = document.getElementsByClassName('today')[0];
   const kcal = document.getElementsByClassName('zeroKcal')[0];
-  const schoolName = document.getElementById('school-name')
+  const schoolName = document.getElementById('school-name');
   fetch(requestUrl)
     .then((res) => res.json())
     .then((json) => {
@@ -25,18 +26,31 @@ const getMealInfo = (schoolCode, { year, month, date }) => {
         meal = meal.replace(/\*/g, '');
         meal = meal.replace(/\./g, ''); // 불필요한 마침표를 제거한다.
         meal = meal.replace('우유', ''); // 우유는 표시하지 않는다.
-        schoolName.innerText = `${json['mealServiceDietInfo'][1].row[0].SCHUL_NM}`;
         document.title = `${json['mealServiceDietInfo'][1].row[0].SCHUL_NM}의 급식`;
         today.innerHTML = `${meal}`;
         kcal.innerText = `${json[
           'mealServiceDietInfo'
         ][1].row[0].CAL_INFO.replace(' Kcal', '')}칼로리`;
         document.getElementById('school-name-div').style.display = 'block';
+        schoolName.innerText = `${json['mealServiceDietInfo'][1].row[0].SCHUL_NM}`;
+        if (
+          json['mealServiceDietInfo'][1].row[0].SCHUL_NM === '서울은평초등학교'
+        ) {
+          schoolName.style.color = 'aqua';
+          schoolName.onclick = function () {
+            easterEgg = easterEgg + 1;
+            if (easterEgg * 1 >= 10) {
+              alert('와! 이걸 찾아내셨군요!');
+            }
+          };
+        }
       }
     })
     .catch((err) => {
+      console.log(err);
       let errorConfirm = confirm(
-        '에러가 발생했습니다. "확인" 버튼을 누르면 자동으로 새로고침 됩니다.' + err,
+        '에러가 발생했습니다. "확인" 버튼을 누르면 자동으로 새로고침 됩니다.' +
+          err,
       );
       if (errorConfirm === true) {
         location.reload();
