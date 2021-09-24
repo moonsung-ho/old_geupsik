c4a26d9457ac57f7f885a513bd5427f2f4c5d273
sungho0205 <moon@7959.ml>
2021-09-24 09:11:10 +0900

    d

--- setting.js
+++ setting.js
@@ -1,15 +1,15 @@
 import schools from './schools.mjs';
 import { setSchoolCode } from './db.mjs';
+let schoolCode = null;
 function click() {
-  let schoolCode = null;
   while (!schoolCode) {
     let input = prompt(
       '학교 이름을 입력하세요. (ex.답십리초, 신길중, 신도림고)',
     );
     schoolCode = schools[input];
-    if (schoolCode) {
-      setSchoolCode(schoolCode);
-    }
   }
+  setSchoolCode(schoolCode);
 }
-document.getElementById("school-change-button").addEventListener('click',click);
+document
+  .getElementById('school-change-button')
+  .addEventListener('click', click);
